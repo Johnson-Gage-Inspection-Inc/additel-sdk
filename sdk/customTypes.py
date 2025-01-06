a202ba96b4f5e3593c89b525031688b07aba3827
Jeff Hall <jeffh@jgiquality.com>
2025-01-06 17:10:08 -0600

    channel, migrated

--- sdk/customTypes.py
+++ sdk/customTypes.py
@@ -1,10 +1,10 @@
-from .DIFunctionChannelConfig import DIFunctionChannelConfig as DFCC
+from .channel import DIFunctionChannelConfig
 from .DIScanInfo import DIScanInfo as DSI
 from .DIModuleInfo import DIModuleInfo as DMI
 from .DIReading import DIReading as DR
 
 class DI:
-    DIFunctionChannelConfig = DFCC
+    DIFunctionChannelConfig = DIFunctionChannelConfig
     DIScanInfo = DSI
     DIModuleInfo = DMI
     DIReading = DR
