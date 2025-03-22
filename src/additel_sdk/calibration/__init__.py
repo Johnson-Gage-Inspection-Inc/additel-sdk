875e6e90868d851562b62b191a6f5f3760000e2d
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-03-22 17:32:50 -0500

    Remove NotImplementedError from Calibration class to allow further implementation

--- src/additel_sdk/calibration/__init__.py
+++ src/additel_sdk/calibration/__init__.py
@@ -4,7 +4,6 @@ from .electricity import Electricity
 
 
 class Calibration:
-    raise NotImplementedError("Calibration module is not implemented yet.")
 
     def __init__(self, parent):
         self.parent = parent
