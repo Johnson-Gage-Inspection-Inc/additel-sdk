f77afd002180ea3d9119df4e7aaa360b5fab10b8
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-03-22 00:10:59 -0500

    Enhance Unit class methods with type hints and update command method for temperature setting

--- src/additel_sdk/notYetImplemented/unit.py
+++ src/additel_sdk/notYetImplemented/unit.py
@@ -1,5 +1,7 @@
 # unit.py - This file contains the class for the Unit commands.
 
+from typing import List
+
 
 # Section 1.8 - Unit commands
 class Unit:
@@ -7,24 +9,20 @@ class Unit:
         self.parent = parent
 
     # 1.8.2
-    def setUnitTemp(self, unit_ID, unit_name):
+    def setUnitTemp(self, unit_ID, unit_name) -> None:
         """Set the temperature of the device
 
         Command:
             UNIT:TEMPerature <unit_ID>|<unit_name>
 
-
         Args:
             Unit: unit name or unit ID unit_name is the character string with quotation
             unit_ID: digit
-
-        Returns:
-            None
         """
-        self.parent.cmd(f"UNIT:TEMPerature {unit_ID}|{unit_name}")
+        self.parent.send_command(f"UNIT:TEMPerature {unit_ID}|{unit_name}")
 
     # 1.8.3
-    def getUnitTemp(self) -> str:
+    def getUnitTemp(self) -> List[str]:
         """Query the temperature of the device
 
         Command:
