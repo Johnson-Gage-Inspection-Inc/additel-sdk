c8cb7da787a10e3fd00769d4c6ef73edd8a9c804
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-03-22 18:15:19 -0500

    Enhance Communicate class: type hint parent parameter and add IP address validation method

--- src/additel_sdk/system/communicate/__init__.py
+++ src/additel_sdk/system/communicate/__init__.py
@@ -3,11 +3,20 @@
 from .wlan import WLAN
 from .ethernet import Ethernet
 from .bluetooth import Bluetooth
+from re import compile
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from src.additel_sdk.system import System
 
 
 class Communicate:
-    def __init__(self, parent):
-        self.parent = parent
+    def __init__(self, parent: "System"):
+        self.parent: "System" = parent
         self.WLAN = WLAN(self)
         self.Ethernet = Ethernet(self)
         self.Bluetooth = Bluetooth(self)
+
+    def validate_ip(self, ip_address: str) -> None:
+        pattern = compile(r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+        if not pattern.match(ip_address):
+            raise ValueError("Invalid IP address format.")
