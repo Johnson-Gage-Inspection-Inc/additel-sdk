749d96100adf424c3f9c6e9b560f7d75bd172522
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-03-22 21:41:01 -0500

    Refactor test_valid_response_status to use fixture for improved clarity and maintainability

--- tests/test_Electricity.py
+++ tests/test_Electricity.py
@@ -1,8 +1,19 @@
+import pytest
 from src.additel_sdk.calibration import Electricity
 
+@pytest.fixture
+def electricity_fixture(device) -> Electricity:
+    return Electricity(device)
 
-def test_valid_response_status(device):
-    elec = Electricity(device)
-    
-    result = elec.get_scan_data()
-    assert result == {'exception_code': '0', 'mode': '0', 'function': '0', 'range': '0', 'status': '0', 'data': None}
+
+def test_valid_response_status(electricity_fixture):
+    expected = {
+        'exception_code': '0',
+        'mode': '0',
+        'function': '0',
+        'range': '0',
+        'status': '0',
+        'data': None
+        }
+    result = electricity_fixture.get_scan_data()
+    assert result == expected, f"Expected {expected} but got {result}"
