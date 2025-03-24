f1a9549541a523b051c30987c037ce34e977c101
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-03-24 01:34:57 -0500

    Refactor TimeTick class to improve TickTime handling and streamline datetime parsing logic

--- src/additel_sdk/time.py
+++ src/additel_sdk/time.py
@@ -1,31 +1,23 @@
 from dataclasses import dataclass, field
-from datetime import datetime, timedelta
-from typing import Union
+from datetime import datetime as dt, timedelta as tΔ
 
 
 @dataclass
 class TimeTick:
-    TickTime: Union[str, int]
-    time: datetime = field(init=False)
+    TickTime: str
+    time: dt = field(init=False)
 
     def __post_init__(self):
-        if isinstance(self.TickTime, int) or (isinstance(self.TickTime, str) and self.TickTime.isdigit()):
-            ticks = int(self.TickTime)
-            self.time = datetime(1, 1, 1) + timedelta(microseconds=ticks // 10)
-        elif isinstance(self.TickTime, str):
-            try:
-                self.time = datetime.strptime(self.TickTime, "%Y-%m-%d %H:%M:%S %f")
-            except ValueError:
-                try:
-                    self.time = datetime.strptime(self.TickTime, "%Y:%m:%d %H:%M:%S %f")
-                except ValueError as e:
-                    raise ValueError("Unrecognized datetime format") from e
+        if '-' in self.TickTime:
+            self.time = dt.strptime(self.TickTime, "%Y-%m-%d %H:%M:%S %f")
+        elif ':' in self.TickTime:
+            self.time = dt.strptime(self.TickTime, "%Y:%m:%d %H:%M:%S %f")
         else:
-            raise TypeError("Input must be an int or str")
+            self.time = dt(1, 1, 1) + tΔ(seconds=int(self.TickTime) / 1e7)
 
     def to_ticks(self) -> int:
         "long timestamp format (ticks since 1/1/0001)"
-        return int((self.time - datetime(1, 1, 1)).total_seconds() * 1e7)
+        return int((self.time - dt(1, 1, 1)).total_seconds() * 1e7)
 
     def to_short_format(self) -> str:
         return self.time.strftime("%Y:%m:%d %H:%M:%S %f")[:-3]
