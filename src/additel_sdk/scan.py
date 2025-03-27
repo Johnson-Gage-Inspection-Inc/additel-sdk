d7e8f5d68ca7e5e7419725e9f7bce356ad3778d3
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-03-27 03:20:14 -0500

    Fix command format for SCAN:STARt in Scan class

--- src/additel_sdk/scan.py
+++ src/additel_sdk/scan.py
@@ -179,7 +179,7 @@ class Scan:
             scan_info (DIScanInfo): The scanning configuration.
         """
         logging.warning("This command has not been tested.")
-        command = f"JSON:SCAN:STARt {scan_info}"
+        command = f"SCAN:STARt {scan_info}"
         self.parent.send_command(command)
 
     def get_configuration_json(self, measure=False) -> DIScanInfo:
